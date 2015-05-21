8c37a87af04532a98a4429c4bca93514e02eaae2
begerjak <begerjak@fel.cvut.cz>
2015-05-22 00:20:19 +0200

    view - table schools

---
+++ src/main/java/controller/MainView.java
@@ -0,0 +1,90 @@
+package controller;
+
+/**
+ * Copyright 2015 IEAP CTU
+ * Author: Jakub Begera (jakub.begera@utef.cvut.cz)
+ */
+
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.stage.Stage;
+import view.VIewController;
+import view.view_model.SchoolView;
+
+import java.io.IOException;
+
+public class MainView extends Application {
+
+    private Stage primaryStage;
+    private ScrollPane rootLayout;
+    private VIewController viewController;
+
+
+    ObservableList<SchoolView> schoolsViewsList = FXCollections.observableArrayList();
+
+    public MainView() {
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("DS semestral project");
+
+        initRootLayout();
+
+
+
+    }
+
+    /**
+     * Initializes the root layout.
+     */
+    public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(VIewController.class.getResource("MainView.fxml"));
+            rootLayout = loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+
+
+
+            viewController = loader.getController();
+            viewController.setMainApp(this);
+            viewController.getTbvSchools().setItems(schoolsViewsList);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+    /**
+     * Returns the main stage.
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+
+
+    }
+
+    public ObservableList<SchoolView> getSchoolsViewsList() {
+        return schoolsViewsList;
+    }
+}
---
+++ src/main/java/view/view_model/SchoolView.java
@@ -0,0 +1,80 @@
+package view.view_model;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+/**
+ * Copyright 2015 IEAP CTU
+ * Author: Jakub Begera (jakub.begera@utef.cvut.cz)
+ */
+public class SchoolView {
+
+    private IntegerProperty id;
+    private  StringProperty name;
+    private  StringProperty desc;
+    private  StringProperty country;
+
+    public SchoolView() {
+    }
+
+    public SchoolView(int id, String name, String desc, String country) {
+        this.id = new SimpleIntegerProperty(id);
+        this.name = new SimpleStringProperty(name);
+        this.desc = new SimpleStringProperty(desc);;
+        this.country = new SimpleStringProperty(country);;
+    }
+
+    public IntegerProperty getIdProp() {
+        return id;
+    }
+
+    public int getId() {
+        return id.get();
+    }
+
+    public IntegerProperty idProperty() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id.set(id);
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public StringProperty nameProperty() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name.set(name);
+    }
+
+    public String getDesc() {
+        return desc.get();
+    }
+
+    public StringProperty descProperty() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc.set(desc);
+    }
+
+    public String getCountry() {
+        return country.get();
+    }
+
+    public StringProperty countryProperty() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country.set(country);
+    }
+}
